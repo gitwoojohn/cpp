a45d2503ef585d1a64ee40c4434be4cf536f8e28
gitwoojohn <gitwoojohn@github.com>
2015-06-26 23:55:55 +0900

    코드 변경

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,10 +2,19 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+   int x, y;
+
+   x = 1;
+   y = 2;
+
+   cout << x + y << endl;
+
 	return 0;
 }
 
